530182baa6ad5cceed845fcc7280c7d0b6dd4084
birju22112002 <69200127+brijesh6353@users.noreply.github.com>
2024-05-20 15:16:10 +0530

    commit

--- controllers/website.js
+++ controllers/website.js
@@ -1,6 +1,6 @@
 /** @format */
 
-// Import the necessary function from the helper
+import Website from "../models/website";
 import { sendNewUserEmail } from "E:\\b\\server\\helpers\\email";
 
 export const contact = async (req, res) => {
@@ -30,6 +30,56 @@ export const contact = async (req, res) => {
     }
   } catch (err) {
     console.error(err);
+    res
+      .status(500)
+      .json({ ok: false, error: "An error occurred. Please try again." });
+  }
+};
+
+export const createPage = async (req, res) => {
+  try {
+    const { page, title, subtitle, fullWidthImage } = req.body;
+    if (!page) {
+      return res
+        .status(400)
+        .json({ ok: false, error: "Page field is required" });
+    }
+
+    const found = await Website.findOne({ page });
+
+    if (found) {
+      // Update
+      const updated = await Website.findOneAndUpdate(
+        { page },
+        { title, subtitle, fullWidthImage },
+        { new: true }
+      );
+      return res.json(updated);
+    } else {
+      // Create
+      const created = await new Website({
+        page,
+        title,
+        subtitle,
+        fullWidthImage,
+      }).save();
+      return res.json(created);
+    }
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ ok: false, error: "An error occurred. Please try again." });
+  }
+};
+
+export const getPage = async (req, res) => {
+  try {
+    const { page } = req.params;
+    const found = await Website.findOne({ page }).populate("fullWidthImage");
+    return res.json(found);
+  } catch (err) {
+    console.log(err);
     res
       .status(500)
       .json({ ok: false, error: "An error occurred. Please try again." });
--- routes/website.js
+++ routes/website.js
@@ -4,9 +4,13 @@ import express from "express";
 
 const router = express.Router();
 
+// middlewares
+import { requireSignin, isAdmin } from "../middlewares";
 // controller
-import { contact } from "../controllers/website";
+import { contact, createPage, getPage } from "../controllers/website";
 
 router.post("/contact", contact);
+router.post("/page", requireSignin, isAdmin, createPage);
+router.get("/page/:page", getPage);
 
 export default router;
